32ed092a71c25dc366d8a4dd80362d994c051f97
Olga <olga.majda@gmail.com>
2024-05-23 14:29:19 -0700

    Add project files.

---
+++ Session01Challenge/App.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Session01Challenge
+{
+    internal class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Session01Challenge/Command.cs
@@ -0,0 +1,70 @@
+﻿#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+#endregion
+
+
+namespace Session01Challenge
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(TextNoteType));
+
+            Transaction t = new Transaction(doc);
+            t.Start("Fizbuzz challenge");
+
+            string result = "";
+
+            XYZ offset = new XYZ(0, 6, 0);
+            XYZ myPoint = new XYZ(0, 0, 0);
+
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result = "FIZZBUZZ";
+                }
+                else if (i % 5 == 0)
+                {
+                    result = "FIZZ";
+                }
+                else if (i % 3 == 0)
+                {
+                    result = "BUZZ";
+                }
+                else
+                {
+                    result = i.ToString();
+                }
+                TextNote MyTexnote = TextNote.Create(doc, doc.ActiveView.Id, myPoint, result, collector.FirstElementId());
+                myPoint = myPoint.Subtract(offset);
+
+            }
+
+
+            t.Commit();
+
+            return Result.Succeeded;
+        }
+    }
+}
